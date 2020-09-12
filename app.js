e26949161193a2fcd62c93b7f2453a05cf8319de
Pramod Jacob Sam <pramodjsam@gmail.com>
2020-09-12 09:28:09 +0530

    Init Commit

---
+++ app.js
@@ -0,0 +1,56 @@
+function animatedForm(){
+	const arrows= document.querySelectorAll(".fa-arrow-down");
+
+	arrows.forEach(function(arrow){
+		arrow.addEventListener("click",function(){
+			const input= arrow.previousElementSibling;
+			const parent= arrow.parentElement;
+			const nextForm= parent.nextElementSibling;
+
+			if(input.type=="text" && validator(input)){
+				nextSlide(parent,nextForm);
+			}else if(input.type =="email" && validateEmail(input)){
+				nextSlide(parent,nextForm);
+			}else if(input.type=="password"&& validator(input)){
+				nextSlide(parent,nextForm)
+			}else{
+				parent.style.animation= "shake 0.5s ease"
+			}
+			parent.addEventListener("animationend",function(){
+				parent.style.animation="";
+			})
+		})
+	})
+}
+
+function validator(user){
+	if(user.value.length<6){
+		error("rgb(189,87,87)")
+	}else{
+		error("rgb(87,189,130)");
+		return true;
+	}
+}
+
+function validateEmail(email){
+	const validation= /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	if(validation.test(email.value)){
+		error("rgb(87,189,130)");
+		return true;
+	}else{
+		error("rgb(189,87,87)");
+	}
+}
+
+function nextSlide(parent,nextForm){
+	parent.classList.add("inactive");
+	parent.classList.remove("active");
+	nextForm.classList.remove("inactive");
+	nextForm.classList.add("active");
+}
+
+function error(color){
+	document.body.style.backgroundColor=color
+}
+
+animatedForm();
